7cce957e400578739d3a65a124cd5c9b58b8686f
Benjamin ALLION <benjamin.allion.pro@gmail.com>
2019-10-11 15:02:44 +0200

    init(): init project.

---
+++ src/main/kotlin/com/example/spring5kotlinboilerplate/Spring5KotlinBoilerplateApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring5kotlinboilerplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring5KotlinBoilerplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<Spring5KotlinBoilerplateApplication>(*args)
+}
